4c30d2085f093928cf27e179f5dfaffa9b72763a
Fuad Muhammed <tchnvirus@gmail.com>
2021-10-02 22:24:19 +0000

    Create dataEntry.php

---
+++ dataEntry.php
@@ -0,0 +1,11 @@
+<?php
+
+$questionnaires = [
+    "Department of study at Accra Technical University (ATU)",
+    "Year of Entry ",
+    "Mode of Entry ",
+    "Year of Completion",
+    "Session of Study",
+    "Programme of study at ATU",
+
+];
