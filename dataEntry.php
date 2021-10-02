2c27c4b08033f98af8293ad32d576b8d02d4315f
Fuad Muhammed <tchnvirus@gmail.com>
2021-10-02 22:31:35 +0000

    Update dataEntry.php

--- dataEntry.php
+++ dataEntry.php
@@ -7,5 +7,13 @@ $questionnaires = [
     "Year of Completion",
     "Session of Study",
     "Programme of study at ATU",
-
+    "Primary reason for choosing ATU",
+    "Primary reason for choosing programme",
+    "Highest qualification obtained at ATU ",
+    "Did you undertake any industrial attachment(s) while at ATU? ",
+    "Name the organisation(s) of industrial attachment ",
+    "Duration of industrial attachment(s)",
+    "Was/were your industrial attachment(s) relevant to your course of study at ATU? ",
+    "How relevant was/were your industrial attachment(s)?",
+    "If 'No', please provide reason(s) for your inability to undertake an industrial attachment ",
 ];
