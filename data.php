5f629362760d40f28c7a37b00c868242fffdbb45
Fuad Muhammed <tchnvirus@gmail.com>
2021-10-03 22:29:50 +0000

    update

---
+++ data.php
@@ -0,0 +1,169 @@
+<?php
+# type
+    # radio
+    # txt
+    # drop
+    
+$questionnaireSet = [
+    "1" => [
+        "question" => "Department of study at Accra Technical University (ATU) ",
+        "type" => "",
+        "options" => [],
+    ],
+    "2" => [
+        "question" => "Year of Entry",
+        "type" => "",
+        "options" => [],
+    ],
+    "3" => [
+        "question" => "Mode of Entry",
+        "type" => "",
+        "options" => [],
+    ],
+    "4" => [
+        "question" => "Year of Completion",
+        "type" => "",
+        "options" => [],
+    ],
+    "5" => [
+        "question" => "Session of Study",
+        "type" => "",
+        "options" => [],
+    ],
+    "6" => [
+        "question" => "Programme of study at ATU",
+        "type" => "",
+        "options" => [],
+    ],
+    "7" => [
+        "question" => "Primary reason for choosing ATU",
+        "type" => "",
+        "options" => [],
+    ],
+    "8" => [
+        "question" => "Primary reason for choosing programme",
+        "type" => "",
+        "options" => [],
+    ],
+    "9" => [
+        "question" => "Highest qualification obtained at ATU",
+        "type" => "",
+        "options" => [],
+    ],
+    "10" => [
+        "question" => "Did you undertake any industrial attachment(s) while at ATU?",
+        "type" => "",
+        "options" => [],
+    ],
+    "11" => [
+        "question" => "Name the organisation(s) of industrial attachment",
+        "type" => "",
+        "options" => [],
+    ],
+    "12" => [
+        "question" => "Duration of industrial attachment(s)",
+        "type" => "",
+        "options" => [],
+    ],
+    "13" => [
+        "question" => "Was/were your industrial attachment(s) relevant to your course of study at ATU?",
+        "type" => "",
+        "options" => [],
+    ],
+    "14" => [
+        "question" => "How relevant was/were your industrial attachment(s)?",
+        "type" => "",
+        "options" => [],
+    ],
+    "15" => [
+        "question" => "If \'No\', please provide reason(s) for your inability to undertake an industrial attachment",
+        "type" => "",
+        "options" => [],
+    ],
+    "16" => [
+        "question" => "Current status/nature of employment",
+        "type" => "",
+        "options" => [],
+    ],
+    "17" => [
+        "question" => "When did you start searching for a job? Whiles in ATU?",
+        "type" => "",
+        "options" => [],
+    ],
+    "18" => [
+        "question" => "How long did it take to secure your first job after graduating from ATU?",
+        "type" => "",
+        "options" => [],
+    ],
+    "19" => [
+        "question" => "What was the most successful method for finding your first job?",
+        "type" => "",
+        "options" => [],
+    ],
+    "20" => [
+        "question" => "How do you perceive the following competencies/skills as affecting recruitment?",
+        "type" => "",
+        "options" => [],
+    ],
+    "21" => [
+        "question" => "To what extent did you acquire the following competencies/skills at ATU?",
+        "type" => "",
+        "options" => [],
+    ],
+    "22" => [
+        "question" => "To what extent do you agree or disagree that the knowledge and skills acquired at ATU are utilised in your current job?",
+        "type" => "",
+        "options" => [],
+    ],
+    "23" => [
+        "question" => "To which sector does your current employment belong?",
+        "type" => "",
+        "options" => [],
+    ],
+    "24" => [
+        "question" => "Job title/position of current employment",
+        "type" => "radio",
+        "options" => [
+            "Director",
+            "Manager",
+            "Supervisor",
+            "Team Leader",
+            "Entry Level"
+        ],
+    ],
+    "25" => [
+        "question" => "To what extent would you choose the same field of study, if you were to decide again?",
+        "type" => "",
+        "options" => [],
+    ],
+    "26" => [
+        "question" => "To what extent are you satisfied with your studies at ATU?",
+        "type" => "",
+        "options" => [],
+    ],
+    "27" => [
+        "question" => "To what extent would you choose ATU, if you were to decide again?",
+        "type" => "",
+        "options" => [],
+    ],
+    "28" => [
+        "question" => "Are you an active member of ATU Alumni?",
+        "type" => "",
+        "options" => [],
+    ],
+    "29" => [
+        "question" => "If \'No\', would you like to join?",
+        "type" => "",
+        "options" => [],
+    ],
+    "30" => [
+        "question" => "Please provide other relevant feedback to ATU for quality assurance purposes.",
+        "type" => "",
+        "options" => [],
+    ],
+    "31" => [
+        "question" => "Please provide the contact information of alumni within your reach to extend participation in the study.",
+        "type" => "",
+        "options" => [],
+    ],
+];
--- dataEntry.php
+++ dataEntry.php
@@ -1,55 +1,65 @@
 <?php
 
-$questionnaires = [
-    "Department of study at Accra Technical University (ATU)",
-    "Year of Entry ",
-    "Mode of Entry ",
-    "Year of Completion",
-    "Session of Study",
-    "Programme of study at ATU",
-    "Primary reason for choosing ATU",
-    "Primary reason for choosing programme",
-    "Highest qualification obtained at ATU ",
-    "Did you undertake any industrial attachment(s) while at ATU? ",
-    "Name the organisation(s) of industrial attachment ",
-    "Duration of industrial attachment(s)",
-    "Was/were your industrial attachment(s) relevant to your course of study at ATU? ",
-    "How relevant was/were your industrial attachment(s)?",
-    "If 'No', please provide reason(s) for your inability to undertake an industrial attachment ",
-    "Current status/nature of employment ",
-    "When did you start searching for a job? Whiles in ATU? ",
-    "How long did it take to secure your first job after graduating from ATU? ",
-    "What was the most successful method for finding your first job? ",
-    "How do you perceive the following competencies/skills as affecting recruitment? ",
-    "To what extent did you acquire the following competencies/skills at ATU? ",
-    "To what extent do you agree or disagree that the knowledge and skills acquired at ATU are utilised in your current job?",
-    "To which sector does your current employment belong?",
-    "Job title/position of current employment ",
-    "To what extent would you choose the same field of study, if you were to decide again?",
-    "To what extent are you satisfied with your studies at ATU? ",
-    "To what extent would you choose ATU, if you were to decide again? ",
-    "Are you an active member of ATU Alumni? ",
-    "If 'No', would you like to join? ",
-    "Please provide other relevant feedback to ATU for quality assurance purposes. ",
-    "Please provide the contact information of alumni within your reach to extend participation in the study.",
-    
-];
-
+include 'data.php';
 
 $conn = mysqli_connect("localhost", "root", "", "survey");
 
 if ($conn !== false) {
-    echo "connected";
-    //for each question
-    foreach ($questionnaires as $question) {
+    $message = "data entered successfully";
 
-        
+    //for each questionnaireSet
+    foreach ($questionnaireSet as $number => $questionSet) {
+
         //insert into questions table
-        $feedback = mysqli_query($conn, "INSERT INTO questions (question) VALUES ('$question')");
-        
-        echo $feedback;
-        
+        $feedback = mysqli_query($conn, "INSERT INTO questions (question) VALUES ('". $questionSet['question'] . "')");
+
+        if (!$feedback) {
+            $message = "data entery error encountered, entering";
+        }
     }
-}else{
+    echo $message;
+} else {
     echo "not connected to database";
 }
+?>
+
+
+// $questionnaires = [
+//     "Department of study at Accra Technical University (ATU)",
+//     "Year of Entry ",
+//     "Mode of Entry ",
+//     "Year of Completion",
+//     "Session of Study",
+//     "Programme of study at ATU",
+//     "Primary reason for choosing ATU",
+//     "Primary reason for choosing programme",
+//     "Highest qualification obtained at ATU ",
+//     "Did you undertake any industrial attachment(s) while at ATU? ",
+//     "Name the organisation(s) of industrial attachment ",
+//     "Duration of industrial attachment(s)",
+//     "Was/were your industrial attachment(s) relevant to your course of study at ATU? ",
+//     "How relevant was/were your industrial attachment(s)?",
+//     "If \'No\', please provide reason(s) for your inability to undertake an industrial attachment ",
+//     "Current status/nature of employment ",
+//     "When did you start searching for a job? Whiles in ATU? ",
+//     "How long did it take to secure your first job after graduating from ATU? ",
+//     "What was the most successful method for finding your first job? ",
+//     "How do you perceive the following competencies/skills as affecting recruitment? ",
+//     "To what extent did you acquire the following competencies/skills at ATU? ",
+//     "To what extent do you agree or disagree that the knowledge and skills acquired at ATU are utilised in your current job?",
+//     "To which sector does your current employment belong?",
+//     "Job title/position of current employment ",
+//     "To what extent would you choose the same field of study, if you were to decide again?",
+//     "To what extent are you satisfied with your studies at ATU? ",
+//     "To what extent would you choose ATU, if you were to decide again? ",
+//     "Are you an active member of ATU Alumni? ",
+//     "If \'No\', would you like to join? ",
+//     "Please provide other relevant feedback to ATU for quality assurance purposes. ",
+//     "Please provide the contact information of alumni within your reach to extend participation in the study.",
+
+// ];
+
+// echo count($questionnaires);
+
+
+
